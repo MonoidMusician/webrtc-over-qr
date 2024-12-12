c9378b53911202d2e7e81ca41ab8e810dbe26c4b
Verity Scheel <monoidmusician@gmail.com>
2024-12-12 00:58:27 -0600

    Canvas?

--- design/app.js
+++ design/app.js
@@ -260,6 +260,7 @@ function app() {
       ]);
       addOutput(HTML.dl(sdps), {}, 'full-width');
     }],
+    'canvas': ['canvas', liveCanvas],
     ...pingModule.commands,
   };
   Object.entries(commands).forEach(([k,v]) => {
@@ -276,7 +277,7 @@ function app() {
       if (typeof data !== 'object')
         data = { messageType: 'text', id: newID(), content: data };
       return data;
-    })
+    });
     scrollManager.adding(() => {
       return messages.map(data => addMessage(...renderMessage(data), 'echo'));
     });
@@ -301,6 +302,7 @@ function app() {
     assert(typeof data.messageType === 'string');
     const classes = data.messageType.split('/').filter(cls=>cls!='*');
     const props = { dataset: { id: data.id } };
+    if (!(data.messageType in messageTypes)) console.error(data);
     return [messageTypes[data.messageType](data, props), props, ...(props.classes||[]), ...classes];
   }
   function addMessage(content, props, ...classes) {
@@ -381,4 +383,69 @@ function app() {
       ours,
     ], { ours, theirs });
   }
+
+  function liveCanvas() {
+    var id = newID();
+    var canvas = HTML.canvas({width:500,height:500});
+    function record(...args) {
+      fragmentDC(dc, JSON.stringify({ type: 'shared', id, args }));
+    }
+    /** @type CanvasRenderingContext2D */
+    var ctx = new Proxy(canvas.getContext('2d'), {
+      get(tgt, name) {
+        const value = Reflect.get(tgt, name);
+        if (typeof value !== 'function' || name.startsWith('is') || name.startsWith('get')) return value;
+        return (...args) => {
+          record('call', name, ...args);
+          return tgt[name](...args);
+        };
+      },
+      set(tgt, name, value) {
+        Reflect.set(tgt, name, value);
+        record('set', name, value);
+        return true;
+      },
+    });
+    scrollManager.adding(() => {
+      return addMessage(canvas, {}, 'echo');
+    });
+    fragmentDC(dc, JSON.stringify({ type: 'message', messageType: 'canvas', id, width: canvas.width, height: canvas.height }));
+    window.ctx = ctx;
+    function coords(/** @type MouseEvent */ ev) {
+      var bb = canvas.getBoundingClientRect();
+      var ret = [
+        coord(ev.clientX, ctx.canvas.width, bb.left, bb.width),
+        coord(ev.clientY, ctx.canvas.height, bb.top, bb.height),
+      ];
+      return ret;
+    }
+    function coord(current, range, min, diff) {
+      return range*(current - min)/diff;
+    }
+    canvas.on.mousedown((ev) => {
+      ctx.beginPath();
+      ctx.moveTo(...coords(ev));
+      ev.preventDefault();
+    });
+    canvas.on.mousemove((ev) => {
+      console.log(ev.buttons);
+      if (!ev.buttons) return;
+      ctx.lineTo(...coords(ev));
+      ctx.stroke();
+    });
+    return canvas;
+  };
+  liveCanvas.guest = function({ id, width, height }) {
+    var canvas = HTML.canvas({width,height});
+    var ctx = canvas.getContext('2d');
+    shared[id] = ({ args }) => {
+      if (args[0] === 'call') {
+        ctx[args[1]](...args.slice(2));
+      } else if (args[0] === 'set') {
+        ctx[args[1]] = args[2];
+      }
+    };
+    return canvas;
+  };
+  messageTypes.canvas = liveCanvas.guest;
 }
